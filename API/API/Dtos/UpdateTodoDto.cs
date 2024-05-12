28b6c65794e02bb2942e0901e00a1ea5cd1ac11f
James Paul Paguirigan <jamespaul.paguirigan@blackfort.ph>
2024-05-12 16:45:16 +0800

    Add C# Web Api Project

---
+++ API/API/Dtos/UpdateTodoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Dtos
+{
+	public class UpdateTodoDto
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Status { get; set; }
+	}
+}
---
+++ API/API/Enums/TodoStatusEnum.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace API.Enums
+{
+	public enum TodoStatusEnum
+	{
+		Todo,
+		[Display(Name = "In Progress")]
+		InProgress,
+		Completed
+	}
+
+	
+	public static class EnumExtensions
+	{
+		// Add method to get the display name
+		public static string GetDisplayName(this Enum value)
+		{
+			return value.GetType()
+						.GetMember(value.ToString())
+						.First()
+						.GetCustomAttribute<DisplayAttribute>()
+						?.Name
+					?? value.ToString();
+		}
+	}
+}
